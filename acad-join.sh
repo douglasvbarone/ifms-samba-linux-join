c5241152789b819523ef15bebc4c2f33953eccce
Douglas Barone <douglasvbarone@gmail.com>
2025-06-23 11:46:35 -0400

    Refactor join script to improve localization and clarity in messages; remove deprecated join.sh file

--- acad-join.sh
+++ acad-join.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
-echo "Installing necessary packages..."
-sudo apt -y install realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit -y >/dev/null
+echo "Instalando pacotes necessários..."
+sudo apt-get -y install realmd libnss-sss libpam-sss sssd sssd-tools adcli samba-common-bin oddjob oddjob-mkhomedir packagekit -y >/dev/null
 clear
-echo "Completed installation of necessary packages. Now printing discovered Kerberos realms..."
+echo "Instalação de pacotes necessários concluída. Agora, imprimindo realms Kerberos descobertos..."
 realm discover
 
-echo "Your domain and its properties should be printed above. If they are not, check DNS config."
+echo "Seu domínio e suas propriedades devem ser impressos acima. Se não forem, verifique a configuração do DNS."
 REALMAD=acad.pp.ifms.edu.br
 REALMDC=acad
 
-NEWHOST="PP-$(tr -dc A-Za-z0-9 </dev/urandom | head -c6)"
+DEFAULTNEWHOST="PP-$(tr -dc A-Za-z0-9 </dev/urandom | head -c6)"
 
-read -p "What is the new hostname? (Ex.: PP-000123) (default: $NEWHOST) " NEWHOST
+read -p "Qual é o novo nome do host? (Ex.: PP-000123) (padrão: $DEFAULTNEWHOST) " NEWHOST
 
 if [ -z "$NEWHOST" ]; then
     # Generate a random hostname if none is provided
-    NEWHOST="PP-$(tr -dc A-Za-z0-9 </dev/urandom | head -c6)"
+    NEWHOST="$DEFAULTNEWHOST"
 fi
 
 shorthost=${HOSTNAME%%.*}
 
 if [ "$NEWHOST" != "$shorthost" ]; then
-    echo "Setting hostname to $NEWHOST"
+    echo "Definindo o nome do host para $NEWHOST"
     sudo hostnamectl set-hostname "$NEWHOST"
 else
-    echo "Hostname is already set to $NEWHOST"
+    echo "O nome do host já está definido como $NEWHOST"
 fi
 
-read -p "What is the domain admin username? " REALMADMIN
+read -p "Qual é o nome de usuário do administrador do domínio? " REALMADMIN
 
 # Set the hostname in /etc/hosts
-echo "Setting hostname in /etc/hosts"
+echo "Definindo o nome do host em /etc/hosts"
 sudo sed -i "s/^127\.0\.1\.1.*/127.0
 .1.1 $NEWHOST $NEWHOST.$REALMAD/" /etc/hosts
 # Set the hostname in /etc/hostname
-echo "Setting hostname in /etc/hostname"
+echo "Definindo o nome do host em /etc/hostname"
 echo "$NEWHOST" | sudo tee /etc/hostname
 
 mkdir /etc/univention
-echo "Connecting to "$REALMDC.$REALMAD" UCS server and pulling UCS config. Password for domain admin will be prompted."
+echo "Conectando-se ao servidor UCS "$REALMDC.$REALMAD" e baixando a configuração do UCS. A senha do administrador do domínio será solicitada."
 ssh -n root@$REALMDC.$REALMAD 'ucr shell | grep -v ^hostname=' >/etc/univention/ucr_master
 echo "master_ip="$REALMDC.$REALMAD"" >>/etc/univention/ucr_master
 chmod 660 /etc/univention/ucr_master
@@ -46,7 +46,7 @@ chmod 660 /etc/univention/ucr_master
 . /etc/univention/ucr_master
 
 # Create an account and save the password
-echo "Creating computer account on "$REALMDC.$REALMAD" UCS server. Password for domain admin will be prompted."
+echo "Criando conta de computador no servidor UCS "$REALMDC.$REALMAD". A senha do administrador do domínio será solicitada."
 password="$(tr -dc A-Za-z0-9_ </dev/urandom | head -c20)"
 ssh -n root@$REALMDC.$REALMAD udm computers/linux create \
     --position "cn=computers,${ldap_base}" \
@@ -56,7 +56,7 @@ ssh -n root@$REALMDC.$REALMAD udm computers/linux create \
 printf '%s' "$password" >/etc/ldap.secret
 chmod 0400 /etc/ldap.secret
 
-echo "Performing domain join operation. Password for domain admin will be prompted."
+echo "Executando a operação de ingresso no domínio. A senha do administrador do domínio será solicitada."
 sudo realm join -v -U "$REALMADMIN" "$REALMAD"
 
 # Create ldap.conf
@@ -65,7 +65,7 @@ echo 'TLS_CACERT /etc/univention/ssl/ucsCA/CAcert.pem
 URI ldap://$ldap_master:7389
 BASE $ldap_base' | sudo tee /etc/ldap/ldap.conf
 
-echo "Activating mkhomedir module..."
+echo "Ativando o módulo mkhomedir..."
 echo 'Name: activate mkhomedir
 Default: yes
 Priority: 900
@@ -76,22 +76,22 @@ sudo pam-auth-update --enable mkhomedir
 sudo systemctl restart sssd
 
 # Do not require users to type @$REALMAD when logging in
-echo "Configuring SSSD to not require domain suffix..."
+echo "Configurando o SSSD para não exigir o sufixo de domínio..."
 sudo sed -i 's/^use_fully_qualified_names = True/use_fully_qualified_names = False/' /etc/sssd/sssd.conf
 #sudo sed -i 's/^fallback_homedir = \/home\/%u/fallback_homedir = \/home\/%u@'$REALMAD'/' /etc/sssd/sssd.conf
 sudo chmod 600 /etc/sssd/sssd.conf
 sudo systemctl restart sssd
 
 # Make admins group members sudoers
-echo "Adding domain admins group to sudoers..."
+echo "Adicionando o grupo de administradores do domínio aos sudoers..."
 echo "%Domain\ Admins ALL=(ALL) ALL" | sudo tee /etc/sudoers.d/99_domain_admins
 sudo chmod 440 /etc/sudoers.d/99_domain_admins
 
 #prompt
-read -r -p "UCS Domain Join Complete! REBOOT NOW? [y/N] " rebootnow
-if [[ "$rebootnow" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-    echo "Rebooting!"
+read -r -p "Ingresso no Domínio UCS Concluído! REINICIAR AGORA? [s/N] " rebootnow
+if [[ "$rebootnow" =~ ^([sS][iI][mM]|[sS])+$ ]]; then
+    echo "Reiniciando!"
     sudo reboot
 else
-    read -p "Reboot not selected. Press any key to finish with script."
+    read -p "Reinicialização não selecionada. Pressione qualquer tecla para finalizar o script."
 fi
